343c808045c62b43f8fd71d003d614dc077d6179
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-05-25 20:57:23 -0300

    Termina requisitos 7 e 8

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import Header from '../../components/Header';
-// import getToken from '../../functions/api/getToken';
-// import shuffleArray from '../../functions/randomArray';
 import './Game.css';
-import Timer from '../../components/Timer';
+// import Timer from '../../components/Timer';
 
 class Game extends React.Component {
   constructor() {
@@ -16,11 +14,27 @@ class Game extends React.Component {
       answers: [],
       question: '',
       colorBorder: false,
+      timer: 30,
     };
   }
 
   componentDidMount = () => {
     this.verifyToken();
+    const second = 1000;
+    this.timerID = setInterval(
+      () => this.tick(), second,
+    );
+  };
+
+  tick = () => {
+    const { timer } = this.state;
+    if (timer > 0) {
+      this.setState((prevState) => ({
+        timer: prevState.timer - 1,
+      }));
+    } else {
+      this.setState({ colorBorder: true });
+    }
   }
 
   handleClick = () => {
@@ -28,7 +42,7 @@ class Game extends React.Component {
       colorBorder: true,
     });
     console.log('oi');
-  }
+  };
 
   verifyToken = async () => {
     const { history } = this.props;
@@ -39,14 +53,15 @@ class Game extends React.Component {
     const half = 0.5;
 
     const savedToken = localStorage.getItem('token');
-    const gameQuestions = await fetch(`https://opentdb.com/api.php?amount=${five}&token=${savedToken}`);
+    const gameQuestions = await fetch(
+      `https://opentdb.com/api.php?amount=${five}&token=${savedToken}`,
+    );
     const questionsReturn = await gameQuestions.json();
 
     if (questionsReturn.response_code === three) {
       localStorage.removeItem('token');
       history.push('/');
     } else {
-      console.log(questionsReturn);
       this.setState({
         questions: questionsReturn,
         category: questionsReturn.results[index].category,
@@ -55,45 +70,71 @@ class Game extends React.Component {
             answer: questionsReturn.results[index].correct_answer,
             id: 'correct-answer',
           },
-          ...questionsReturn.results[index].incorrect_answers.map((iAnswer, i) => ({
-            answer: iAnswer,
-            id: `wrong-answer-${i}`,
-          }))].sort(() => half - Math.random()),
+          ...questionsReturn.results[index].incorrect_answers.map(
+            (iAnswer, i) => ({
+              answer: iAnswer,
+              id: `wrong-answer-${i}`,
+            }),
+          ),
+        ].sort(() => half - Math.random()),
         question: questionsReturn.results[index].question,
       });
     }
+  };
+
+  border = (answer, correctA) => {
+    if (answer === correctA) {
+      return 'green';
+    }
+    return 'red';
+  };
+
+  selectAnswer = () => {
+    this.setState({ colorBorder: true });
   }
 
   render() {
-    const { questions, answers, category, question, colorBorder } = this.state;
+    const { questions, answers, category, question, colorBorder, timer } = this.state;
+    const correctAnswerElement = answers.find(
+      (answer) => answer.id === 'correct-answer',
+    );
+    const correctAnswer = () => {
+      if (correctAnswerElement !== undefined) {
+        return correctAnswerElement.answer;
+      }
+      return '';
+    };
     return (
       <article>
-        <Timer />
+        {/* <Timer /> */}
+        <span>
+          {timer}
+        </span>
         <Header />
-        {questions === {} ? <h1>Loading</h1> : (
+        {questions === {} ? (
+          <h1>Loading</h1>
+        ) : (
           <section>
-            <h1 data-testid="question-category">
-              {category}
-            </h1>
-            <h3 data-testid="question-text">
-              {question}
-            </h3>
+            <h1 data-testid="question-category">{category}</h1>
+            <h3 data-testid="question-text">{question}</h3>
             <section data-testid="answer-options">
               {answers.map((a, i) => (
                 <button
                   data-testid={ a.id }
                   key={ i }
                   type="button"
-                  className={ colorBorder && answers
-                    .some((element) => element.correct_answer === a)
-                    ? 'green' : 'red' }
-                  onClick={ () => this.handleClick() }
+                  className={
+                    colorBorder ? this.border(a.answer, correctAnswer()) : ''
+                  }
+                  onClick={ this.selectAnswer }
+                  disabled={ colorBorder }
                 >
                   {a.answer}
                 </button>
               ))}
             </section>
-          </section>)}
+          </section>
+        )}
       </article>
     );
   }
