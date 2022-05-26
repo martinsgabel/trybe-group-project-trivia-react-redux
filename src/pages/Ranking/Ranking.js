8d987a4fb1d03b41be065a060bbf8371cbac020a
asafworld <gabrielasaf10@gmail.com>
2022-05-26 16:44:40 -0300

    Adiciona o requisito 18

--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <article>
         {/* <Header /> */}
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Logout
+        </button>
       </article>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
